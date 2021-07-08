adbb15a40f4794c0a4cd0c4394e587bd7e799508
Aditya Tiwari <72137687+AdityaTiwari64@users.noreply.github.com>
2021-07-08 20:46:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'index.html'));
+})
+
+users = [];
+io.on('connection', function(socket){
+    console.log('A user Connected');
+    socket.on('setUsername',function(data){
+        console.log(data);
+
+        if(users.indexOf(data)> -1){
+            socket.emit('userExists',data + ' username is taken! Try using other username')
+        }
+        else{
+            users.push(data);
+            socket.emit('userSet',{username:data})
+        }
+    })
+    socket.on('msg',function(data){
+        io.emit('newmessage',data)
+    })
+})
+
+
+http.listen('3000', function(){
+    console.log('App Listening On Port 3000')
+})
